5107cd4308e2a6c5ca0f874aac5a2d0dd2aed930
joelhulen <joelhulen@gmail.com>
2024-12-30 12:26:33 -0500

    Enable codespaces and devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Install NPM modules for Woodgrove Web UI
+echo "Installing NPM modules for Woodgrove Web UI"
+# pushd ./src/userportal
+# npm install
+# npx next telemetry disable
+# popd

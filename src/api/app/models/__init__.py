e32590264466d46bdef39a39eee51bb292605520
Chris Pietschmann <chris@build5nines.com>
2024-12-30 19:55:10 -0500

    Update __init__.py

--- src/api/app/models/__init__.py
+++ src/api/app/models/__init__.py
@@ -4,4 +4,5 @@ from .completion_request import CompletionRequest
 from .invoice import Invoice
 from .list_response import ListResponse
 from .sow import Sow, SowEdit
+from .validation_request import ValidationRequest
 from .vendor import Vendor

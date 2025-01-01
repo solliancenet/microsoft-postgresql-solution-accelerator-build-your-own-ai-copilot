8086bab2655ec6f2bd11cbfe03f139b03bb988db
Chris Pietschmann <chris@build5nines.com>
2025-01-01 16:47:25 -0500

    Fix Postgresql connection string username for app running with Managed Identity

--- src/api/app/services/database_service.py
+++ src/api/app/services/database_service.py
@@ -2,7 +2,7 @@ import asyncpg
 import urllib.parse
 from azure.identity.aio import DefaultAzureCredential
 import jwt
-import aiohttp
+import os
 
 class DatabaseService:
     """Class to manage the connection to the Azure Database for PostgreSQL server."""
@@ -22,35 +22,13 @@ class DatabaseService:
     async def __get_username(self, token):
         """get username from token or Entra if token does not have username"""
         decoded_token = jwt.decode(token, options={"verify_signature": False})
+        # When running as a User, this will be populated
         username = decoded_token.get("preferred_username") or decoded_token.get("upn")
-        # if username is None
         if username is None:
-            # call Entra to get userPrincipalName
-            username = await self.__get_authenticated_entity_name()
+            # When running as an App with Managed Identity configured, this will be populated instead
+            # load app Identity principal name from environment variable
+            username = os.getenv('AZURE_IDENTITY_NAME')
         return username
-
-    async def __get_authenticated_entity_name(self) -> str:
-        """Get the name of the authenticated entity."""
-        try:
-            # Get an access token for Microsoft Graph
-            token = await self.credential.get_token("https://graph.microsoft.com/.default")
-            headers = {
-                "Authorization": f"Bearer {token.token}"
-            }
-            
-            # Call the Microsoft Graph /me endpoint
-            async with aiohttp.ClientSession() as session:
-                async with session.get("https://graph.microsoft.com/v1.0/me", headers=headers) as response:
-                    if response.status == 200:
-                        result = await response.json()
-                        # Return user or service principal name
-                        return result.get("userPrincipalName") or result.get("displayName")
-                    else:
-                        error = await response.text()
-                        raise Exception(f"Error fetching entity: {error}")
-        except Exception as e:
-            return f"Failed to retrieve entity name: {str(e)}"
-
 
     async def __get_connection_uri(self):
         """Get the connection URI for the Azure Database for PostgreSQL server."""
@@ -66,4 +44,5 @@ class DatabaseService:
         password = token
 
         db_uri = f"postgresql://{urllib.parse.quote_plus(username)}:{password}@{self.host_name}:5432/{self.database_name}?sslmode={sslmode}"
+
         return db_uri

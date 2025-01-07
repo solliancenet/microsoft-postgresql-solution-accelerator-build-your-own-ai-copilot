14759d2bbd2be1b51c603518b4f3d01c174d5c3f
HeyMo0sh <hamishwatson8@outlook.com>
2025-01-07 21:00:48 +1300

    New tables and MSA ingestion

--- infra/aca/scripts/deploy-database-tables.sql
+++ infra/aca/scripts/deploy-database-tables.sql
@@ -1,8 +1,7 @@
 /*  File to idempotently load table DDL for Claims Data  */
 
 -- Vendors table: information about companies (e.g., tags, industry codes, preferences)
-CREATE TABLE IF NOT EXISTS vendors
-(
+CREATE TABLE IF NOT EXISTS vendors (
     id BIGSERIAL PRIMARY KEY,
     name text NOT NULL,
     address text NOT NULL,
@@ -13,14 +12,25 @@ CREATE TABLE IF NOT EXISTS vendors
     metadata jsonb -- additional information about the vendor
 );
 
+-- MSA table; information about payment terms, special clauses, or additional legal notes
+CREATE TABLE IF NOT EXISTS msas (
+    id BIGSERIAL PRIMARY KEY,
+    title text NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    additional_info JSONB -- Stores special clauses, terms, etc.
+);
+
 -- Statement of work table; information about deliverables, milestones, or resource allocations.
 CREATE TABLE IF NOT EXISTS sows (
     id BIGSERIAL PRIMARY KEY,
-    msa_itle text NOT NULL,
+    msa_id BIGINT NOT NULL,
+    msa_title text NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     budget DECIMAL(18,2) NOT NULL,
     document text NOT NULL,
+    FOREIGN KEY (msa_id) REFERENCES msas (id) ON DELETE CASCADE,
     details JSONB -- Flexible for deliverables, milestones, and notes
 );
 
@@ -35,11 +45,22 @@ CREATE TABLE IF NOT EXISTS invoices (
     invoice_details JSONB -- Tax info, discounts, or itemized breakdown
 );
 
--- MSA table; information about payment terms, special clauses, or additional legal notes
-CREATE TABLE IF NOT EXISTS msas (
+
+-- Milestones table
+CREATE TABLE IF NOT EXISTS milestones (
     id BIGSERIAL PRIMARY KEY,
-    title text NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE,
-    additional_info JSONB -- Stores special clauses, terms, etc.
+    sow_id BIGINT NOT NULL,
+    milestone_name text NOT NULL,
+    due_date DATE,
+    FOREIGN KEY (sow_id) REFERENCES sows (id) ON DELETE CASCADE
+);
+
+-- Deliverables table
+CREATE TABLE IF NOT EXISTS deliverables (
+    id SERIAL PRIMARY KEY,
+    milestone_id BIGINT NOT NULL,
+    deliverable_name VARCHAR(100) NOT NULL,
+    description TEXT,
+    amount NUMERIC(10, 2),
+    FOREIGN KEY (milestone_id) REFERENCES milestones (id) ON DELETE CASCADE
 );

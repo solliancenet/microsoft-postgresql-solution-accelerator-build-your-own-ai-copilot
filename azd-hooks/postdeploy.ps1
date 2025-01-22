9462d9f6ffa8476067374d7bc6d2bba4575f5bc6
Chris Pietschmann <chris@build5nines.com>
2025-01-22 12:44:57 -0500

    Update postdeploy.ps1 - move extensions install before schema creation

--- azd-hooks/postdeploy.ps1
+++ azd-hooks/postdeploy.ps1
@@ -42,6 +42,24 @@ az postgres flexible-server ad-admin create `
 Write-Host "Added $username as an Admin on PostgreSQL Server"
 
 # ##############################################################################
+# Configure PostgreSQL Server Extensions with OpenAI and Vector Extensions
+# ##############################################################################
+Write-Host "Configuring PostgreSQL Server Extensions for OpenAI and Vector..."
+# Load script
+$sqlScript = Get-Content -Path "./scripts/sql/setup_azure_ai.sql" -Raw
+# Replace environment variable placeholders
+$sqlScript = $sqlScript.Replace('${env:AZURE_OPENAI_ENDPOINT}', "${env:AZURE_OPENAI_ENDPOINT}").Replace('${env:AZURE_OPENAI_KEY}', "${env:AZURE_OPENAI_KEY}")
+# Run script
+az postgres flexible-server execute `
+          --admin-user "$username" `
+          --admin-password "$token" `
+          --name "${env:POSTGRESQL_SERVER_NAME}" `
+          --database-name "${env:POSTGRESQL_DATABASE_NAME}" `
+          --querytext $sqlScript
+
+Write-Host "PostgreSQL Server Extensions Configured"
+
+# ##############################################################################
 # Create Database Schema
 # ##############################################################################
 Write-Host "Configuring Database Schema..."
@@ -73,23 +91,6 @@ az postgres flexible-server execute `
 
 Write-Host "Database Permissions Granted to API App Managed Identity"
 
-# ##############################################################################
-# Configure PostgreSQL Server Extensions with OpenAI and Vector Extensions
-# ##############################################################################
-Write-Host "Configuring PostgreSQL Server Extensions for OpenAI and Vector..."
-# Load script
-$sqlScript = Get-Content -Path "./scripts/sql/setup_azure_ai.sql" -Raw
-# Replace environment variable placeholders
-$sqlScript = $sqlScript.Replace('${env:AZURE_OPENAI_ENDPOINT}', "${env:AZURE_OPENAI_ENDPOINT}").Replace('${env:AZURE_OPENAI_KEY}', "${env:AZURE_OPENAI_KEY}")
-# Run script
-az postgres flexible-server execute `
-          --admin-user "$username" `
-          --admin-password "$token" `
-          --name "${env:POSTGRESQL_SERVER_NAME}" `
-          --database-name "${env:POSTGRESQL_DATABASE_NAME}" `
-          --querytext $sqlScript
-
-Write-Host "PostgreSQL Server Extensions Configured"
 
 
 

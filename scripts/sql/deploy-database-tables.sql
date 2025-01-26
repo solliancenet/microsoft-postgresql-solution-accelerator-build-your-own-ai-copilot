73bcd2ce748074957de8d74f945f6149bafde826
HeyMo0sh <hamishwatson8@outlook.com>
2025-01-27 11:21:08 +1300

    Inserts for milestone table, DDL change

--- scripts/sql/deploy-database-tables.sql
+++ scripts/sql/deploy-database-tables.sql
@@ -70,14 +70,14 @@ SELECT 'SOW-WoodgroveBank-2024-001',
 WHERE NOT EXISTS (SELECT 1 FROM sows WHERE number = 'SOW-WoodgroveBank-2024-001');
 
 INSERT INTO sows (number, vendor_id, start_date, end_date, budget, document, metadata)
-SELECT 'WoodgroveBank-SOW-001',
+SELECT 'SOW-WoodgroveBank-001',
        2, --Contoso DevOps Services
        '2024-06-01',
        '2025-11-30',
        75000.00,
        '2/sow/Statement_of_Work_Contoso_DevOps_Services_Woodgrove_Bank_20240601.pdf',
        '{}'
-WHERE NOT EXISTS (SELECT 1 FROM sows WHERE number = 'WoodgroveBank-SOW-001');
+WHERE NOT EXISTS (SELECT 1 FROM sows WHERE number = 'SOW-WoodgroveBank-001');
 
 INSERT INTO sows (number, vendor_id, start_date, end_date, budget, document, metadata)
 SELECT 'SOW-LP-WGB-001',
@@ -89,15 +89,15 @@ SELECT 'SOW-LP-WGB-001',
        '{}'
 WHERE NOT EXISTS (SELECT 1 FROM sows WHERE number = 'SOW-LP-WGB-001');
 
-INSERT INTO sows (number, vendor_id, start_date, end_date, budget, document, metadata)
-SELECT 'WWE-WoodgroveBank-SOW-001',
+INSERT INTO sows (number, vendor_id, sow_id, start_date, end_date, budget, document, metadata)
+SELECT 'SOW-WoodgroveBank-WWE-001',
        4, --Wide World Engineering
        '2024-10-01',
        '2025-09-30',
        60000.00,
        '4/sow/Statement_of_Work_Wide_World_Engineering_Woodgrove_Bank_20241001.pdf',
        '{}'
-WHERE NOT EXISTS (SELECT 1 FROM sows WHERE number = 'WWE-WoodgroveBank-SOW-001');
+WHERE NOT EXISTS (SELECT 1 FROM sows WHERE number = 'SOW-WoodgroveBank-WWE-001');
 
 INSERT INTO sows (number, vendor_id, start_date, end_date, budget, document, metadata)
 SELECT 'SOW-2024-WoodgroveBank-001',
@@ -114,14 +114,19 @@ CREATE TABLE IF NOT EXISTS invoices (
     id BIGSERIAL PRIMARY KEY,
     number text NOT NULL,
     vendor_id BIGINT NOT NULL,
+    sow_id BIGINT NOT NULL,
     amount DECIMAL(18,2) NOT NULL,
     invoice_date DATE NOT NULL,
     payment_status VARCHAR(50) NOT NULL,
     document text NOT NULL,         -- document details
     content text,                   --  for invoice embeddings
     metadata JSONB,                 --  for additional metadata
-    FOREIGN KEY (vendor_id) REFERENCES vendors (id) ON DELETE CASCADE
+    FOREIGN KEY (vendor_id) REFERENCES vendors (id) ON DELETE CASCADE,
+    FOREIGN KEY (sow_id) REFERENCES sows (id) ON DELETE CASCADE
 );
+
+
+
 
 -- Milestones table
 CREATE TABLE IF NOT EXISTS milestones (
@@ -129,9 +134,21 @@ CREATE TABLE IF NOT EXISTS milestones (
     sow_id BIGINT NOT NULL,
     name text NOT NULL,
     status VARCHAR(50) NOT NULL,
-    due_date DATE,
     FOREIGN KEY (sow_id) REFERENCES sows (id) ON DELETE CASCADE
 );
+
+-- Insert milestones 
+INSERT INTO milestones (sow_id, name, status)
+VALUES
+    (1,'Monitoring of resources','Completed'),
+    (1,'Cost Management','Completed'),
+    (1,'Automated Scaling','Completed'),
+    (1,'Maintenance Practices','Completed'),
+    (1,'Issue Resolution','In Progress'),
+    (2,'CI/CD Pipelines','Completed'),
+    (2,'Containerized Applications','In Progress'),
+    (2,'Cloud Monitoring','In Progress')
+
 
 -- Deliverables table
 CREATE TABLE IF NOT EXISTS deliverables (
@@ -141,6 +158,7 @@ CREATE TABLE IF NOT EXISTS deliverables (
     description TEXT,
     amount NUMERIC(10, 2),
     status VARCHAR(50) NOT NULL,
+    due_date DATE NOT NULL,
     FOREIGN KEY (milestone_id) REFERENCES milestones (id) ON DELETE CASCADE
 );
 

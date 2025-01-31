b34d34759edc92dcd9e8d8ad3501fa24b0da2193
kylebunting <kyle.bunting@gmail.com>
2025-01-30 18:30:53 -0700

    Setting components to pre-AI state

--- src/userportal/src/pages/dashboard/dashboard.jsx
+++ src/userportal/src/pages/dashboard/dashboard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import CopilotChat from '../../components/CopilotChat';
 
 const Dashboard = () => {
   return (
@@ -7,8 +6,6 @@ const Dashboard = () => {
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 className="h2">Dashboard</h1>
       </div>
-      
-      <CopilotChat />
 
     </div>
   );
